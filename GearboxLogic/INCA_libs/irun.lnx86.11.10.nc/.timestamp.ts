d4675b8e9e606fc71dbf8df22e38af793a2280cd
hwsimu05 <hwsimu05@lsra05.(none)>
2013-11-26 13:44:15 +0100

    deleted some temp files

--- GearboxLogic/INCA_libs/irun.lnx86.11.10.nc/.timestamp.ts
+++
@@ -1,3 +0,0 @@
-1385396125 /home/hwsimu05/VerilogExercise/common/clk_gen.v
-1385469158 /home/hwsimu05/VerilogExercise/GearboxLogic/gearbox.v
-1385469731 /home/hwsimu05/VerilogExercise/GearboxLogic/gearbox_tb.v
