c2b4ff4b799a46503925255a7cf670d7950eae77
TM90 <tobias_markus@gmx.net>
2013-12-13 14:09:31 +0100

    modified RAM fifo and finished TB

--- RAMBasedFIFO/RAMfifo_tb.v
+++ RAMBasedFIFO/RAMfifo_tb.v
@@ -1,5 +1,5 @@
 //Authors: Sebastian Wittka, Tobias Markus
-module module_name ();
+module RAMfifo_tb ();
 	
 	parameter WIDTH = 32;
 	parameter DEPTH = 12;
@@ -10,59 +10,54 @@ module module_name ();
 	reg shift_out;
 	reg [WIDTH-1:0] wdata;
 	wire full;
-	wire emtpy;
+	wire empty;
 	wire [WIDTH-1:0] rdata;
-
+	integer i;
 	clk_gen #(.period(PERIOD)) clk_gen_I (.clk_out(clk));
 	RAMfifo #(.WIDTH(WIDTH),.DEPTH(DEPTH)) RAMfifo_I (.clk(clk),.res_n(res_n), .shift_in(shift_in),
 			.shift_out(shift_out),.wdata(wdata),.full(full),.empty(empty),.rdata(rdata));
 
 	task empty_fifo;
-		input empty;
-		output shift_out;
-	begin
-		while(empty == 1)
 		begin
+			while(empty == 0)
+			begin
+				@(negedge(clk));
+				shift_in <= 0;
+				shift_out <= 1;
+			end
 			@(negedge(clk));
-			shift_out <= 1;
+			shift_out <= 0;
 		end
-		@(negedge(clk));
-		shift_out <= 0;
 	endtask
 
 	task fill_fifo;
-		input wordsToFill;
-		output shift_in;
-		output wdata;
-	begin
-		if (wordsToFill>(2**DEPTH)) 
+		input integer wordsToFill;
 		begin
-			$error "You want to fill the FIFO with to many words.\n 
-					wordsToFill= %d words\n FIFO_size= %d words\n"	
+		if(wordsToFill > 2**DEPTH)
+		begin
+			$display("ERROR: You want to fill to many bytes into the FIFO");
+			$stop;
 		end
-		else
-		begin
-
-			for (i = 0; i < wordsToFill; i = i +1) 
+			for (i=0;i<wordsToFill;i=i+1) 
 			begin
-    	  		@(negedge(clk));
-    	  			shift_in <= 1;
-    	  			wdata <= $random;
-    		end
+				@(negedge(clk));
+			    shift_in <= 1;
+			    shift_out <= 0;
+			    wdata <= $random;	
+			end
 		end
 	endtask
 
 	initial 
 	begin
-		res_n <= 0;
+		res_n <= 0; //is this .res_n a pointer ???
 		shift_in <= 0;
 		shift_out <= 0;
-		wdata <= 0;
 		#100
 		res_n <= 1;
-		fill_fifo(1024,shift_in,wdata);
+		fill_fifo(1024);
 		#50
-		empty_fifo(empty,shift_out);
+		empty_fifo();
 		$stop;
 	end
 endmodule
