d03a4da9d173c8f7136f82c4cff06b89301aaf3b
Sebastian Wittka <sebastian.wittka@gmail.com>
2013-12-16 15:49:33 +0100

    In ISE RegBasedFifo cannot be added to TB

--- RegBasedFIFO/regb_fifo.v
+++ RegBasedFIFO/regb_fifo.v
@@ -3,11 +3,11 @@
 module fifo #(parameter N=5, parameter WIDTH=4)
 (input wire clk,
 input wire res_n,
-input wire in,
+input wire[WIDTH-1:0] wdata,
 input wire shift_in,
 input wire shift_out,
-output wire out,
-output wire empty_n,
+output wire[WIDTH-1:0] rdata,
+output wire empty,
 output wire full);
 wire [N:0] unit_out[WIDTH-1:0], unit_so[WIDTH-1:0], unit_empty_n_reg_out,
 unit_empty_n_reg_before_in, unit_empty_n_reg_next_in, unit_empty_n_out, unit_g_in;
@@ -15,7 +15,7 @@ generate
   genvar i;
   for(i=0; i<N; i=i+1)
     begin: fifo
-      fifo_unit #(.WIDTH(WIDTH)) fifo_unit_I(.clk(clk), .res_n(res_n), .si(in), .so(unit_so[i]),
+      fifo_unit #(.WIDTH(WIDTH)) fifo_unit_I(.clk(clk), .res_n(res_n), .si(wdata), .so(unit_so[i]),
       .empty_n_before(unit_g_in[i]), .shift_out(shift_out),
 		.empty_n_reg_next(unit_empty_n_reg_next_in[i]),
       .empty_n_reg_before(unit_empty_n_reg_before_in[i]), .shift_in(shift_in), .out(unit_out),
@@ -26,9 +26,9 @@ generate
 		assign g_in[i+1] = unit_empty_n_out[i];
     end
   assign unit_so[0] = {WIDTH{1'b0}};
-  assign out = unit_out[N-1];
+  assign rdata = unit_out[N-1];
   assign unit_empty_n_reg_before_in[0] = 1'b1;
-  assign empty_n = unit_empty_n_reg_out[N-1];
+  assign empty = ~unit_empty_n_reg_out[N-1];
   assign empty_n_reg_next_in[N-1] = 1;
   assign g_in[0] = shift_in;
   assign full = unit_empty_n_reg_out[0];
--- RegBasedFIFO/regb_fifo_tb.v
+++ RegBasedFIFO/regb_fifo_tb.v
@@ -1,26 +1,61 @@
 //Authors: Sebastian Wittka, Tobias Markus
 module fifo_tb();
+
+  parameter WIDTH = 8;
+  parameter DEPTH = 5;
+  parameter PERIOD = 10;
+  
   wire clk;
   wire full;
   wire empty;
-  wire[WIDTH-1:0] out;
+  wire[WIDTH-1:0] rdata;
   reg res_n;
   reg shift_in;
-  reg[WIDTH-1:0] in;
+  reg[WIDTH-1:0] wdata;
   reg shift_out;
-  assign WIDTH = 4;
   
-  task_tb task_tb();
-  clk_gen clk_gen_I(clk);
-  fifo #(.WIDTH(WIDTH), .N(5)) fifo_I(.clk(clk), .res_n(res_n), .full(full), .shift_in(shift_in),
-  .in(in), .empty(empty), .shift_out(shift_out), .out(out));
-endmodule
+  clk_gen #(.period(PERIOD)) clk_gen_I (.clk_out(clk));
+  fifo #(.WIDTH(WIDTH), .N(DEPTH)) fifo_I(.clk(clk), .res_n(res_n), .full(full), .shift_in(shift_in),
+  .wdata(wdata), .empty(empty), .shift_out(shift_out), .rdata(rdata));
 
-module task_tb ();
-  task task_fifo();
-  
-  endtask
-  begin
-    task_fifo();
-  end
+	task empty_fifo;
+		begin
+			while(empty == 0)
+			begin
+				@(negedge(clk));
+				shift_in <= 0;
+				shift_out <= 1;
+			end
+			@(negedge(clk));
+			shift_out <= 0;
+		end
+	endtask
+
+	task full_fifo;
+		begin
+			while(full == 0) 
+			begin
+				@(negedge(clk));
+			    shift_in <= 1;
+			    shift_out <= 0;
+			    wdata <= $random;	
+			end
+      @(negedge(clk));
+      shift_in <= 0;
+		end
+	endtask
+
+	initial 
+	begin
+		res_n <= 0;
+		shift_in <= 0;
+		shift_out <= 0;
+		#100
+		res_n <= 1;
+		full_fifo();
+		#50
+		empty_fifo();
+		$stop;
+	end
+
 endmodule
