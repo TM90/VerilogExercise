eca6a19f821aabda90a7937ebefc8613e6b2e24d
TM90 <tobias_markus@gmx.net>
2014-01-08 23:22:19 +0100

    worked on Arbiter first finished version (not tested yet)

--- Arbiter/Arbiter.v
+++ Arbiter/Arbiter.v
@@ -1,23 +1,20 @@
 //Authors: Sebastian Wittka, Tobias Markus
 module Arbiter
-	#(
-	    parameter WIDTH = 8
-	)
     (
         input clk,
 	    input res_n,
-        input[WIDTH-1:0] input0,
-        input[WIDTH-1:0] input1,
-        input[WIDTH-1:0] input2,
         input req0,
         input req1,
         input req2,
-        output reg grant0,
-        output reg grant1,
-        output reg grant2
+        output wire grant0,
+        output wire grant1,
+        output wire grant2
     );
     
     reg[2:0] state_reg;
+    assign grant0 = state_reg(0);
+    assign grant1 = state_reg(1);
+    assign grant2 = state_reg(2);
 
     always @(posedge clk or negedge res_n) 
 	begin
@@ -27,7 +24,74 @@ module Arbiter
         end
         else
         begin
-
+            case state_reg
+                3'b000:
+                    if (req0 == 1) 
+                    begin
+                        state_reg <= 3'b001;
+                    end
+                    else if (req1 == 1)
+                    begin
+                        state_reg <= 3'b010;
+                    end
+                    else if (req2 == 1)
+                    begin
+                        state_reg <= 3'b100;   
+                    end
+                3'b001:
+                    if (req0 == 0 && req1 == 0 && req2 == 0) 
+                    begin
+                        state_reg <= 3'b000;
+                    end
+                    if (req0 == 1)
+                    begin
+                        state_reg <= 3'b001;
+                    end
+                    else if (req1 == 1)
+                    begin
+                        state_reg <= 3'b010;    
+                    end
+                    else if (req2 == 1)
+                    begin
+                        state_reg <= 3'b100;
+                    end
+                3'b010:
+                    if (req0 == 0 && req1 == 0 && req2 == 0) 
+                    begin
+                        state_reg <= 3'b000;
+                    end
+                    if (req0 == 1)
+                    begin
+                        state_reg <= 3'b001;
+                    end
+                    else if (req1 == 1)
+                    begin
+                        state_reg <= 3'b010;    
+                    end
+                    else if (req2 == 1)
+                    begin
+                        state_reg <= 3'b100;
+                    end
+                3'b100:
+                    if (req0 == 0 && req1 == 0 && req2 == 0) 
+                    begin
+                        state_reg <= 3'b000;
+                    end
+                    if (req0 == 1)
+                    begin
+                        state_reg <= 3'b001;
+                    end
+                    else if (req1 == 1)
+                    begin
+                        state_reg <= 3'b010;    
+                    end
+                    else if (req2 == 1)
+                    begin
+                        state_reg <= 3'b100;
+                    end
+                default: 
+                    state_reg <= 3'b000;
+            endcase
         end
     end
 endmodule 
