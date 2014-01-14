445538e2e0124fc0c0f011808f324e50fe56ac0d
hwsimu05 <hwsimu05@creek04.ziti.uni-heidelberg.de>
2014-01-14 12:11:22 +0100

    deleted some files

--- Arbiter/INCA_libs/irun.lnx86.11.10.nc/files.ts
+++
@@ -1,3 +0,0 @@
-1389694604 /home/hwsimu05/VerilogExercise/Arbiter/Arbiter.v
-1389694605 /home/hwsimu05/VerilogExercise/Arbiter/Arbiter_tb.v
-1386670360 /home/hwsimu05/VerilogExercise/common/clk_gen.v
--- RegBasedFIFO/INCA_libs/irun.lnx86.11.10.nc/files.ts
+++
@@ -1,4 +0,0 @@
-1386670360 /home/hwsimu05/VerilogExercise/common/clk_gen.v
-1389696216 /home/hwsimu05/VerilogExercise/RegBasedFIFO/regb_fifo.v
-1389697622 /home/hwsimu05/VerilogExercise/RegBasedFIFO/regb_fifo_tb.v
-1389697001 /home/hwsimu05/VerilogExercise/RegBasedFIFO/regb_fifo_unit.v
